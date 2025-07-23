a8ebf9b9cbff5e534dcdb73d03c4e57c91cae2bc
Alex Bordon <Alexbordongarrigos@gmail.com>
2025-07-23 00:15:38 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/publish/page.tsx
+++ src/app/publish/page.tsx
@@ -1,6 +1,7 @@
 
 "use client";
 
+import * as React from "react";
 import { useState, useMemo, useCallback } from "react";
 import { useRouter } from "next/navigation";
 import { useUser } from "@/context/UserContext";
